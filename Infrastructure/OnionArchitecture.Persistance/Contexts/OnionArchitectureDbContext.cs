3860ff45e8b6eee3ffb49addd62627b063806a79
emrah karaman <ekaraman89@hotmail.com>
2022-04-12 15:01:32 +0300

    intercepter ile createddate eklemesi yapildi

--- Infrastructure/OnionArchitecture.Persistance/Contexts/OnionArchitectureDbContext.cs
+++ Infrastructure/OnionArchitecture.Persistance/Contexts/OnionArchitectureDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Domain.Entities.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,22 @@ namespace OnionArchitecture.Persistance.Contexts
         DbSet<Customer> Customers { get; set; }
         DbSet<Order> Orders { get; set; }
         DbSet<Product> Products { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var items = ChangeTracker.Entries<BaseEntity>();
+            foreach (var item in items)
+            {
+                _ = item.State switch
+                {
+                    EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
+                    //EntityState.Detached => throw new NotImplementedException(),
+                    //EntityState.Unchanged => throw new NotImplementedException(),
+                    //EntityState.Deleted => throw new NotImplementedException(),
+                    //EntityState.Modified => throw new NotImplementedException(),
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
